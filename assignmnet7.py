78c268aa48a5477e333818b3492f77776961b98e
vansssa <sqa-pt@KKBOXdeMacBook-Air-80.local>
2015-12-24 09:58:32 +0800

    調整

--- assignmnet7.py
+++ assignmnet7.py
@@ -1,9 +1,6 @@
 
 def sum_func():
-	li=[ i**2 if i % 2 == 1 else -i**2 for i in range(1,11)]
-	return sum(li)
+    li = [i**2 if i % 2 == 1 else -i**2 for i in range(1, 11)]
+    return sum(li)
 
-print sum_func() 
-
-
-
+print sum_func()
--- test_assignment7.py
+++ test_assignment7.py
@@ -1,11 +1,12 @@
 import unittest
 from assignmnet7 import sum_func
 
+
 class TestAssignment7(unittest.TestCase):
-	def test_sum_func(self) :
-		self.assertEqual(sum_func() , -55)
+
+    def test_sum_func(self):
+        self.assertEqual(sum_func(), -55)
 
 
 if __name__ == '__main__':
     unittest.main()
-
